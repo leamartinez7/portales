ec57e3f83a6f07e6b728931fe35a370a746dde3d
Melanie Molina <melanie.molina@davinci.edu.ar>
2025-04-30 22:48:01 -0300

    productos agregados

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -5,36 +5,44 @@
  */
 ?>
 
-
 <x-layout>
 
     <x-slot:title>Productos</x-slot>
 
     <h1 class="mb-4">Todos los productos</h1>
     @auth
-        <p><a class="mb-3" href="{{ route('productos.crear') }}">Cargar nuevo producto</a></p>
+        <a class="mb-3" href="{{ route('productos.crear') }}">Cargar nuevo producto</a>
     @endauth
 
-
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
         @foreach($productos as $producto)
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ Storage::url($producto->imagen) }}" class="card-img-top" alt="{{ $producto->nombre }}" style="height: 200px; object-fit: cover;">
-                    <div class="card-body d-flex flex-column">
-                        <h2 class="h4 card-title">{{ $producto->nombre }}</h2>
-                        <p class="card-text text-muted">{{ $producto->categoria }}</p>
-                        <p class="card-text fw-bold text-success">${{ $producto->precio }}</p>
-                        <div class="mt-auto">
-                            <a href="{{ route('productos.ver', ['id' => $producto->producto_id]) }}" class="btn btn-primary w-100">Ver detalles</a>
-                        </div>
+        <a href="{{ route('productos.ver', ['id' => $producto->producto_id]) }}" class="text-decoration-none text-reset">
+        <div class="col">
+                <div class="h-100 d-flex flex-column">
+                    <!-- Imagen -->
+                    <img src="{{ Storage::url($producto->imagen) }}" 
+                        alt="{{ $producto->nombre }}" 
+                        class="img-fluid w-100" 
+                        style="aspect-ratio: 1 / 1.1; object-fit: contain; border-radius: 0;">
+
+                    <!-- Información -->
+                    <div class="mt-2">
+                        <h2 class="h6 m-0">{{ $producto->nombre }}</h2>
+                        <p class="fw-bold m-0">${{ number_format($producto->precio, 0, ',', '.') }}</p>
+                        <p class="text-muted small">
+                            ${{ number_format($producto->precio * 0.75, 0, ',', '.') }} con Transferencia o depósito bancario
+                        </p>
                     </div>
                 </div>
             </div>
+        </a>
+
         @endforeach
     </div>
 
+
 </x-layout>
+
 
 
 
